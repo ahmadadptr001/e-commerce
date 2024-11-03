6dd37197d594b47f2ae6951c4cefa968bddc27d9
ahmadadptr001 <ahmadadptr@gmail.com>
2024-11-03 09:24:21 +0700

    ganti ke mode hash

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>,
+  document.getElementById('root')
 );
