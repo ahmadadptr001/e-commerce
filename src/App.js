97d185091834d91eebc962768075a6a04b030aa3
ahmadadptr001 <ahmadadptr@gmail.com>
2024-11-03 12:04:39 +0700

    add basename

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ function App() {
 
         <Route
           path="/shop-more"
+          basename="/e-commerce"
           element={
             <>
               <Navigation onSearchChange={handleInputChange} />
